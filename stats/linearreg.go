bdd350119fb751339ff73e1230f2bfa4a999cc13
shaokoth <shadrackomondi005@gmail.com>
2024-08-22 21:32:42 +0300

    bug fixing

--- stats/linearreg.go
+++ stats/linearreg.go
@@ -1,6 +1,6 @@
 package linear_stats
 
-func LinearReg(x, y []float64) (float64, float64)  {
+func LinearReg(x, y []float64) (float64, float64) {
 	sumx := 0.0
 	sumy := 0.0
 	sumxy := 0.0
@@ -9,15 +9,15 @@ func LinearReg(x, y []float64) (float64, float64)  {
 	a := 0.0
 	b := 0.0
 
-	for i := 0; i <= len(x)-1; i++ {
-		N := len(x)
+	for i := 0; i < len(x); i++ {
+		N := float64(len(x))
 		sumx += float64(x[i])
 		sumy += float64(y[i])
 		sumxy += float64(x[i]) * float64(y[i])
 		sumx2 += float64(x[i]) * float64(x[i])
 		sumy2 += float64(y[i]) * float64(y[i])
-		b = (float64(N)*sumxy - sumx*sumy) / (float64(N)*sumx2 - sumx*sumx)
-		a = (sumy - b*sumx) / float64(N)
+		a = (sumy / N) - (((N*sumxy - sumx*sumy) / (N*sumx2 - sumx*sumx)) * ((sumx) / N))
+		b = (N*sumxy - sumx*sumy) / (N*sumx2 - sumx*sumx)
 	}
 	return a, b
 }
