e8c30e90888d672dbce342b02703ab2107256009
shaokoth <shadrackomondi005@gmail.com>
2024-08-24 18:49:37 +0300

    add functionality to calculate and print the Linear Regression Line

--- stats/linearreg.go
+++ stats/linearreg.go
@@ -16,8 +16,10 @@ func LinearReg(x, y []float64) (float64, float64) {
 		sumxy += float64(x[i]) * float64(y[i])
 		sumx2 += float64(x[i]) * float64(x[i])
 		sumy2 += float64(y[i]) * float64(y[i])
-		a = (sumy / N) - (((N*sumxy - sumx*sumy) / (N*sumx2 - sumx*sumx)) * ((sumx) / N))
+		// Calculates the slope
 		b = (N*sumxy - sumx*sumy) / (N*sumx2 - sumx*sumx)
+		// Calculates the y-intercept
+		a = (sumy / N) - (b * ((sumx) / N))
 	}
 	return a, b
 }
